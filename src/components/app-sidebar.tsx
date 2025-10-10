24699efabe765769cbdae3005766a668c4aad15d
顾华林 <guhl@zts.com.cn>
2025-10-10 10:24:55 +0800

    style: 更改图标底色

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -105,7 +105,7 @@ export function AppSidebar() {
               isActive={pathname === "/"}
             >
               <Link href="/">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-blue-500 text-white">
                   <Wrench className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
